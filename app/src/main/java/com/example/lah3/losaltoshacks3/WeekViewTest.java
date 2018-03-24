6392ab0b11940a503758ace2622aefacd268b976
razrslyr <sharpedo75@gmail.com>
2018-03-24 16:47:12 -0700

    acheese

--- app/src/main/java/com/example/lah3/losaltoshacks3/WeekViewTest.java
+++ app/src/main/java/com/example/lah3/losaltoshacks3/WeekViewTest.java
@@ -1,14 +1,25 @@
 package com.example.lah3.losaltoshacks3;
 
 import android.content.Intent;
+import android.graphics.RectF;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class WeekViewTest extends AppCompatActivity {
 
@@ -17,6 +28,14 @@ public class WeekViewTest extends AppCompatActivity {
     boolean isOpen = false;
     TextView taskName;
     TextView eventName;
+    WeekView ashish;
+    List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+
+    int times = 0;
+
+
+
+    public static WeekViewEvent selectedEvent = new WeekViewEvent();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +53,8 @@ public class WeekViewTest extends AppCompatActivity {
 
         taskName = (TextView) findViewById(R.id.taskTextView);
         eventName = (TextView) findViewById(R.id.eventTextView);
+
+
 
         fab_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,10 +97,98 @@ public class WeekViewTest extends AppCompatActivity {
             }
         });
 
+        Toast.makeText(this, "ashish rao", Toast.LENGTH_LONG).show();
+        ashish = (WeekView)findViewById(R.id.ashish);
 
+        MonthLoader.MonthChangeListener mMonthChangeListener = new MonthLoader.MonthChangeListener() {
+            @Override
+            public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+                // Populate the week view with some events.
+
+
+
+                if(times > 0) {
+                    events.clear();
+                }
+
+
+                WeekViewEvent event = new WeekViewEvent(1, "Kill\nall\nthe\nmoon\nmen" + times, 2018, 3, 25, 0, 0, 2018, 3, 26, 14, 0);
+
+                events.add(event);
+                event.setColor(getResources().getColor(R.color.darkOrange));
+
+                WeekViewEvent event2 = new WeekViewEvent(2, "Kill\n all\n the\n moon\n men\n again" + times, 2018, 3, 26, 15, 0, 2018, 3, 26, 20, 0);
+                events.add(event2);
+                events.add(event);
+
+
+
+
+                times++;
+
+
+                Log.e("times month change", Integer.toString(times));
+                Log.e("Events Size", Integer.toString(events.size()));
+
+
+
+
+                return events;
+            }
+
+        };
+
+        ashish.setMonthChangeListener(mMonthChangeListener);
+
+
+
+
+        ashish.setOnEventClickListener(new WeekView.EventClickListener() {
+            @Override
+            public void onEventClick(WeekViewEvent event, RectF eventRect) {
+                Toast.makeText(getApplicationContext(), event.getName(), Toast.LENGTH_LONG).show();
+                selectedEvent = event;
+            }
+        });
 
 
 
 
     }
+
+
+    /*
+
+  \
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_week_view_test);
+
+
+
+
+
+        ashish.setMonthChangeListener(mMonthChangeListener);
+
+
+
+
+        ashish.setOnEventClickListener(new WeekView.EventClickListener() {
+            @Override
+            public void onEventClick(WeekViewEvent event, RectF eventRect) {
+                Toast.makeText(getApplicationContext(), event.getName(), Toast.LENGTH_LONG).show();
+                selectedEvent = event;
+            }
+        });
+
+
+
+    }
+
+
+}
+
+     */
 }
