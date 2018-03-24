3a2d80f3394c67c6e3f335888cc26207852c6abf
praneethguduguntla <praneethg347@gmail.com>
2018-03-24 14:01:43 -0700

    firebase authentication added

--- app/src/main/java/com/example/lah3/losaltoshacks3/MainActivity.java
+++ app/src/main/java/com/example/lah3/losaltoshacks3/MainActivity.java
@@ -1,13 +1,86 @@
 package com.example.lah3.losaltoshacks3;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText memailField;
+    private EditText mpasswordField;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        memailField = (EditText) findViewById(R.id.emailField);
+        mpasswordField = (EditText) findViewById(R.id.passwordField);
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null){
+            Intent i = new Intent(this, WeekViewTest.class);
+
+            startActivity(i);
+        }else{
+            return;
+        }
+    }
+
+    private void createAccount(String email, String password) {
+       final Intent i = new Intent(this, WeekViewTest.class);
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                            startActivity(i);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, task.getException().toString(), Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
+
+    }
+
+
+
+    public void onCreateAccount(View v){
+
+        createAccount(memailField.getText().toString().trim(), mpasswordField.getText().toString().trim());
+
+    }
+
+
 }
